7b84d01b9a4520eb6254e878fde2450c31f74dde
Mariano Vera Causich <95258268+MarianoVera22@users.noreply.github.com>
2025-05-31 10:19:28 -0300

    References

---
+++ CsharpAdvanced/Reference/Program.cs
@@ -0,0 +1,34 @@
+﻿// por referencia asociada a ref
+int num = 1;
+Console.WriteLine(num);
+change(ref num);
+Console.WriteLine(num);
+
+
+// los objetos se mandan siempre por referencia default
+var number = new Number() { Num = 1 };
+Console.WriteLine(number.Num);
+changeObject(number);
+Console.WriteLine(number.Num);
+
+// excepcion string
+string name = "Pedro";
+Console.WriteLine(name);
+changeString(name);
+Console.WriteLine(name);
+
+void change(ref int num2) {
+    num2= 2;
+}
+
+void changeObject(Number n) {
+    n.Num = 2;
+}
+
+void changeString(string str)
+{
+    str = "Juan";
+}
+public class Number { 
+    public decimal Num {  get; set; }
+}
