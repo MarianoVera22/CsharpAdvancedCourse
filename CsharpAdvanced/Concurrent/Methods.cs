3a2c427d6c6baa640854de93cffad1ec9589f158
Mariano Vera Causich <95258268+MarianoVera22@users.noreply.github.com>
2025-06-10 15:51:57 -0300

    Concurrency - Task

---
+++ CsharpAdvanced/Concurrent/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurrent
+{
+    public static class Methods
+    {
+        public static async Task Wait(int miliseconds)
+        {
+            Console.WriteLine("Comienza la espera");
+            await Task.Delay(miliseconds);
+            Console.WriteLine("Termina la espera");
+        }
+
+        public static async Task<double> AddAsync(double number1, double number2)
+        {
+            await Task.Delay(1000);
+            return number1 + number2;
+        }
+    }
+}
--- CsharpAdvanced/Concurrent/Program.cs
+++ CsharpAdvanced/Concurrent/Program.cs
@@ -19,5 +19,79 @@
 
 //Console.WriteLine("--------------Fin de mi programa--------------");
 
-
 // Async await
+
+using Concurrent;
+
+//Console.WriteLine("--------------Inicia mi programa--------------");
+
+
+//var task = Methods.Wait(1000);
+//Console.WriteLine("Hago algo mas");
+//await task; // Debe ser await para que respete el await de la funcion
+
+//Console.WriteLine("--------------Fin de mi programa--------------");
+
+
+// TResult
+
+var taskResult = Methods.AddAsync(3, 5);
+
+//Console.WriteLine("Mientras voy a comer");
+var result = await taskResult;
+
+//Console.WriteLine("Voy a banarme");
+//Console.WriteLine(result);
+
+// ContinueTask
+
+var tasks = Task.Run(() =>
+{
+    //Console.WriteLine("--------------Inicia mi tarea secuencial--------------");
+
+    var resultTask = Methods.AddAsync(5, 10);
+
+    //Console.WriteLine("--------------Finaliza mi tarea secuencial--------------");
+
+    return resultTask.Result;
+
+}).ContinueWith((resultTask) =>
+{
+    //Console.WriteLine("--------------Inicia mi segunda tarea secuencial--------------");
+
+    var result = resultTask.Result;
+    Console.WriteLine($"El resultado es {result}");
+
+    Task.Delay(2000).Wait();
+
+    //Console.WriteLine("--------------Finaliza mi segunda tarea secuencial--------------");
+});
+
+await tasks;
+
+//Console.WriteLine("--------------Fin de las tareas secuenciales--------------");
+
+
+// WhenAll
+
+List<Task> waitTasks = new List<Task>()
+{
+    Methods.Wait(1000),
+    Methods.Wait(3000)
+};
+
+await Task.WhenAll(waitTasks);
+
+Console.WriteLine("Terminaron las tareas de espera");
+
+List<Task<double>> addTasks = new List<Task<double>>() {
+    Methods.AddAsync(10,10), Methods.AddAsync(20,20),Methods.AddAsync(30,30)
+};
+
+double[] results = await Task.WhenAll(addTasks);
+
+foreach (var res in results) {
+    Console.WriteLine("Resultado:" + res);
+}
+
+Console.WriteLine("Terminaron las tareas de suma");
